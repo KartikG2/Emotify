0aea50ce1ca596a204776a82862c31880a26d0d6
KartikG2 <kartikgangavati2004@gmail.com>
2025-11-25 15:44:27 +0530

    updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://moodsync-2-1.onrender.com', 'http://localhost:5173'],
+  origin: ['https://emootify.netlify.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
